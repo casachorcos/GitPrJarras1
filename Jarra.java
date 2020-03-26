f91a3cf3c9896d390d5ea97db561dd11c3bde452
nicocasamayor <nicocasamayor@uma.es>
2020-03-26 12:14:14 +0100

    Programador 4 prJarras

--- Jarra.java
+++ Jarra.java
@@ -1,15 +1,36 @@
 public class Jarra {//variables de instancia
     private int cantidad;
     private int capacidad;
-    public Jarra(int capacidadInicial){
-        if(capacidadInicial<=0){
+
+    public Jarra(int capacidadInicial) {
+        if (capacidadInicial <= 0) {
             throw new RuntimeException("ERROR: no se pueden crear jarras “ +
                     "con capacidad menor o igual que cero.");
         }
-        this.capacidad=capacidadInicial;
+        this.capacidad = capacidadInicial;
     }
     // Aquí vendrán después los métodos creados en la siguiente fase
-    public void metodoComunitario(){
+    //Programador4
+    public Object clone() throws CloneNotSupportedException {
+        return (Jarra) super.clone();
+    }
+
+    public bool equals(Jarra otraJarra) {
+        boolean eqs = false;
+        if (null == otraJarra) {
+            eqs = false;
+        } else if (this == otraJarra) {
+            eqs = true;
+        } else if (otraJarra instanceOf Jarra.class){
+            eqs = this.capacidad == otraJarra.capacidad &&
+                    this.cantidad == otraJarra.cantidad;
+        } else{
+            eqs = false;
+        }
+        return eqs;
+    }
+
+    public void metodoComunitario() {
 // Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
     }
